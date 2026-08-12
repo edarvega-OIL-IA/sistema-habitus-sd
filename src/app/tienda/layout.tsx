30e5d47c6e1982af028d9c04a01954c80cb80630
edarvega-OIL-IA <edarvega@gmail.com>
2026-08-12 20:31:38 -0300

    feat: agrupar Instagram y WhatsApp como iconos flotantes visibles

--- src/app/tienda/layout.tsx
+++ src/app/tienda/layout.tsx
@@ -1,12 +1,12 @@
 // Ruta destino: C:\Users\Usuario\Documents\sistema-habitus-sd\src\app\tienda\layout.tsx
 import { CarritoProvider } from '@/components/tienda/CarritoContext'
-import BotonWhatsAppFlotante from '@/components/tienda/BotonWhatsAppFlotante'
+import RedesSocialesFlotantes from '@/components/tienda/RedesSocialesFlotantes'
 
 export default function TiendaLayout({ children }: { children: React.ReactNode }) {
   return (
     <CarritoProvider>
       {children}
-      <BotonWhatsAppFlotante />
+      <RedesSocialesFlotantes />
     </CarritoProvider>
   )
 }
